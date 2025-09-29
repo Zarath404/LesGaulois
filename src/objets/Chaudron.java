910cbd9e21d90e53de8be1eaba64a3b9e62a957c
Zarath404 <david92.joffre@gmail.com>
2025-09-29 19:54:48 +0200

    presque fini tp 1

--- src/objets/Chaudron.java
+++ src/objets/Chaudron.java
@@ -1,16 +1,12 @@
 package objets;
 
 public class Chaudron {
-	private int quantitePotion;
-	private int forcePotion;
-	
-	public Chaudron(int quantitePotion, int forcePotion) {
-		this.quantitePotion = quantitePotion;
-		this.forcePotion = forcePotion;
-	}
+	private int quantitePotion = 0;
+	private int forcePotion = 0;
 	
 	public void remplirChaudron(int quantite, int forcePotion) {
-		
+		this.quantitePotion = quantite;
+		this.forcePotion = forcePotion;
 	}
 	
 	public boolean resterPotion() {
--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,10 +1,14 @@
 package personnages;
 
+import objets.Chaudron;
+
 public class Druide {
 	private String nom;
 	private int force;
+	private Chaudron chaudron;
 	
 	public Druide(String nom, int effetPotion) {
+		chaudron = new Chaudron();
 		this.nom = nom;
 		this.force = force;
 	}
@@ -26,7 +30,9 @@ public class Druide {
 	}
 	
 	private void fabriquerPotion(int quantité, int forcePotion) {
-		
+		chaudron.remplirChaudron(quantité, forcePotion);
+		this.parler("J'ai concocté " + quantité + "dose de potin magique,"
+				+ "elle a une force de " + forcePotion);
 	}
 	
 	private void booster(Gaulois gaulois) {
