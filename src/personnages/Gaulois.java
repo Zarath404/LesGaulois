b592a6eaaed4b4560fc2a85b3f2477ea62cb0d43
Zarath404 <david92.joffre@gmail.com>
2025-09-30 15:02:03 +0200

    TP 1 teminé

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,12 +3,18 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	private int effetPotion = 1;
+	
 	public Gaulois(String nom, int force) {
 	this.nom = nom;
 	this.force = force;
 	}
 	public String getNom() {
 	return nom;
+	}
+	
+	public void setEffetPotion(int effetPotion) {
+		this.effetPotion = effetPotion;
 	}
 	public void parler(String texte) {
 	System.out.println(prendreParole() + "\"" + texte + "\"");
@@ -29,9 +35,13 @@ public class Gaulois {
 	
 	public void frapper(Romain romain) {
 		String nomRomain = romain.getNom();
-		System.out.println(nom + "envoie un grand coup dans la mâchoire de" + nomRomain);
-		int forceCoup = force/3;
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + nomRomain);
+		int forceCoup = force * effetPotion;
+		forceCoup = forceCoup/3;
 		romain.recevoirCoup(forceCoup);
+		if (effetPotion > 1) {
+			effetPotion = effetPotion - 1;
+		}
 	}
 }
 
